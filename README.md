2a5f30687699272e60c201184f3bec1709c89f13
qingsheng.sun <somereason@gmail.com>
2021-09-23 10:01:09 +0800

    update

--- README.md
+++ README.md
@@ -1,8 +1,33 @@
 # README
 
+a demo for graalvm and springboot
+
+## commands
+
+generate config file by agent
+
+> java -agentlib:native-image-agent=config-output-dir=src/main/resources/META-INF/native-image -jar target/graalvm-demo-0.0.1-SNAPSHOT.jar
+
+then request following pages for a better coverage of dynamic accesses
+
+```sh
+curl http://localhost:9001/
+curl http://localhost:9001/random_car
+curl http://localhost:9001/indexjson
+```
+
+package
+
+> mvn clean package -Dmaven.test.skip=true
+
+run
+
+> target/graalvm-demo
+
 ## TODO
 
 * use freemarker
 * use profile in pom(note: try to remove spring-boot-maven-plugin)
 * config logs
 * add static file, and read in app
+
--- src/main/java/sqs/example/graalvmdemo/HomeController.java
+++ src/main/java/sqs/example/graalvmdemo/HomeController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.util.Optional;
 import java.util.Random;
@@ -29,5 +30,10 @@ public class HomeController {
         Random r = new Random();
         Optional<CarEntity> car = carRepository.findById(r.nextInt((int) count));
         return car.orElse(CarEntity.nullObject());
+    }
+
+    @GetMapping("/")
+    private ModelAndView index() {
+        return new ModelAndView("index");
     }
 }
